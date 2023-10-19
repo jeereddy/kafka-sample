cf25fd16fc12fc62e3144e6df2d5e9e4aa9b4101
srinathJeereddy <j.srinath6323@gmail.com>
2023-10-19 12:33:41 -0500

    initial commit.

---
+++ src/test/java/com/omsai/kafkasample/KafkaSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omsai.kafkasample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
